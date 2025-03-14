e0e0a9001a51529321134d9c48b3e45ea8e8e96d
pphenomen <dzamikhovz@gmail.com>
2025-03-14 23:59:02 +0300

    5.2: квадратное уравнение

--- FunctionsRecursion/FunctionsRecursion/Program.fs
+++ FunctionsRecursion/FunctionsRecursion/Program.fs
@@ -1,1 +1,13 @@
 ﻿printfn("Hello World!")
+
+let solveQuadratic a b c =
+    let discriminant = b*b - 4.0*a*c
+    if discriminant > 0.0 then
+        let x1 = (-b + sqrt discriminant) / (2.0*a)
+        let x2 = (-b - sqrt discriminant) / (2.0*a)
+        x1, x2
+    elif discriminant = 0.0 then
+        let x = -b / (2.0*a)
+        x, x
+    else 
+        nan, nan
