359ac2220b328c5f56c28122773b64bb0b039250
pphenomen <dzamikhovz@gmail.com>
2025-03-15 16:21:24 +0300

    5.2: квадратное уравнение 2.0

--- FunctionsRecursion/FunctionsRecursion/Program.fs
+++ FunctionsRecursion/FunctionsRecursion/Program.fs
@@ -5,9 +5,14 @@ let solveQuadratic a b c =
     if discriminant > 0.0 then
         let x1 = (-b + sqrt discriminant) / (2.0*a)
         let x2 = (-b - sqrt discriminant) / (2.0*a)
-        x1, x2
+        Some(x1, x2)
     elif discriminant = 0.0 then
         let x = -b / (2.0*a)
-        x, x
+        Some(x, x)
     else 
-        nan, nan
+        None
+
+let res = solveQuadratic 1.0 2.0 -3.0
+match res with
+    | Some(x1, x2) -> printf $"Решение: {x1}, {x2}" 
+    | None -> printfn "Нет вещественных корней"
