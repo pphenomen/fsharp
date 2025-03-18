d6f4f7997c98bd2d4b96370ca35810ff7f6031f0
pphenomen <dzamikhovz@gmail.com>
2025-03-18 16:51:38 +0300

    5.3: каррирование

--- FunctionsRecursion/FunctionsRecursion/Program.fs
+++ FunctionsRecursion/FunctionsRecursion/Program.fs
@@ -1,4 +1,6 @@
-﻿printfn("Hello World!")
+﻿open System
+
+printfn("Hello World!")
 
 let solveQuadratic a b c =
     let discriminant = b*b - 4.0*a*c
@@ -14,5 +16,30 @@ let solveQuadratic a b c =
 
 let res = solveQuadratic 1.0 2.0 -3.0
 match res with
-    | Some(x1, x2) -> printf $"Решение: {x1}, {x2}" 
+    | Some(x1, x2) -> printfn $"Решение: {x1}, {x2}"
+    | Some(x1, x2) when x1 = x2 -> printfn $"Уравнение имеет один корень: {x1}"
     | None -> printfn "Нет вещественных корней"
+
+let circleSquare r : float =
+    let pi = 3.14159
+    pi * r * r
+
+let cylinderVolume h r : float =
+    h * circleSquare r
+
+let curryCylinderVolume h = 
+    fun r -> cylinderVolume h r
+
+[<EntryPoint>]
+let main argv =
+    printf "Введите высоту: "
+    let h = Console.ReadLine() |> float
+
+    printf "Введите радиус: "
+    let r = Console.ReadLine() |> float
+
+    let curriedVolume = curryCylinderVolume h
+    let vol = curriedVolume r
+    printfn "Объем цилиндра: %f" vol
+
+    0
