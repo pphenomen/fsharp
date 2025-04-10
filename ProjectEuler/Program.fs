301ffead940356a854f185577307a3684290d8d9
pphenomen <pphenomen44@gmail.com>
2025-04-10 21:10:38 +0300

    ИЗ1: решение и тесты

--- ProjectEuler/Program.fs
+++ ProjectEuler/Program.fs
@@ -8,3 +8,13 @@ let findA n =
         if current = 0 then k
         else loop (k + 1) ((current * 10 + 1) % n)
     loop 1 1
+
+let findMinN limit =
+    Seq.initInfinite id |> Seq.filter isCoprimeWith10 |> Seq.find (fun n -> findA n > limit)
+
+[<EntryPoint>]
+let main args =
+    printfn "A(n)>10 = %d" (findMinN 10)
+    printfn "A(n)>1000000 = %d" (findMinN 1000000)
+
+    0
