c09359a47bb8aa411d97d7b2156eb95bcce2cae2
pphenomen <pphenomen44@gmail.com>
2025-04-11 19:11:14 +0300

    7.4: агент (MailboxProcessor)

---
+++ OOP/Task4/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+
+// алгебраический тип сообщений
+type EchoMessage =
+    | Say of string
+    | Exit
+
+// агент
+let echoAgent = MailboxProcessor.Start(fun inbox ->
+    let rec loop () = async {
+        let! msg = inbox.Receive()
+        match msg with
+        | Say text ->
+            printfn "Получено сообщение: %s" text
+            return! loop()
+        | Exit ->
+            printfn "Агент завершает работу."
+            return ()
+    }
+    loop ()
+)
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите сообщение ('exit' для выхода):"
+    let mutable running = true
+    while running do
+        let input = Console.ReadLine()
+        if input = "exit" then
+            echoAgent.Post Exit
+            running <- false
+        else
+            echoAgent.Post(Say input)
+    0
