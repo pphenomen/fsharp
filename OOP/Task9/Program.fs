f928365ec4b3e043e0016d85996b3c5655b9bbbd
pphenomen <pphenomen44@gmail.com>
2025-04-12 17:19:20 +0300

    7.9: WinForms (массив из 100 чисел, кратных 13 или 17)

---
+++ OOP/Task9/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+open System.Windows.Forms
+
+let form = new Form(Text = "WinForm", Width = 300, Height = 400)
+
+let label = new Label(Top = 10, Left = 20, Width = 250, Height = 40, Text = "Массив из первых 100 натуральных чисел,\nкратных 13 или 17:")
+let listBox = new ListBox(Top = 50, Left = 20, Width = 240, Height = 300)
+form.Controls.Add(label)
+form.Controls.Add(listBox)
+
+let array =
+    Seq.initInfinite (fun num -> num + 1) 
+    |> Seq.filter (fun x -> x % 13 = 0 || x % 17 = 0)
+    |> Seq.truncate 100
+    |> Seq.toArray
+
+for n in array do
+    listBox.Items.Add(n)
+
+[<STAThread>]
+[<EntryPoint>]
+let main _ =
+    Application.Run(form)
+    0
